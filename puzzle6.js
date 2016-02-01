d25032abd301f3e601ef7d1f69e1ffdd6676628a
kdelosreyes <kdelosreyes@cca.edu>
2016-02-01 08:35:40 -0800

    Update puzzle6.js

--- puzzle6.js
+++ puzzle6.js
@@ -1,11 +1,29 @@
 right();
-up();
-up();
-up();
-if (getColor() == "red") {
-  left();
-} else {
-  right();
+if (getColor()=="red") {
+    for(var i=0; i<3;i++) {
+  	up();
 }
+if (getColor()=="red"){
+    left();
 up();
+}
+else{
+  right();
+  up();
+}
+}
+else {
+  for (var i=0; i<3; i++) {
+    down();
+    }
+  if (getColor()=="red") {
+    left();
+    down();
+  }
+  else{
+    right();
+    down();
+  }
+}
+
 
